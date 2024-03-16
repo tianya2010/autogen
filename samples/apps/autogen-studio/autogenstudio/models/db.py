cfe9a960c7a19e8e755f931d8e451bfc9832cbaa
Victor Dibia <victordibia@microsoft.com>
2024-03-15 20:34:32 -0700

    rewrite endpoint to use new ORM implementation v1

--- samples/apps/autogen-studio/autogenstudio/models/db.py
+++ samples/apps/autogen-studio/autogenstudio/models/db.py
@@ -1,94 +1,115 @@
-from typing import Any, Callable, Dict, Literal, Optional, Union
-from altair import List
-from sqlmodel import JSON, Column, DateTime, Field, SQLModel, func
+from typing import Any, Callable, Dict, Literal, Optional, Union, List
+from sqlmodel import JSON, Column, DateTime, Field, SQLModel, func, Relationship
 from datetime import datetime
 
 
-class BaseDBModel(SQLModel):
+class Message(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default=datetime.now(), sa_column=Column(
         DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
     updated_at: datetime = Field(default=datetime.now(), sa_column=Column(
         DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
     user_id: Optional[str] = None
-
-
-class Message(BaseDBModel, table=True):
     role: str
     content: str
-    session_id: str
+    session_id: Optional[int] = Field(default=None, foreign_key="session.id")
+    workflow_id: Optional[int] = Field(default=None, foreign_key="workflow.id")
+    connection_id: Optional[str] = None
     meta: Optional[Dict] = Field(default={}, sa_column=Column(JSON))
 
 
-class Session(BaseDBModel, table=True):
+class Session(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
+    updated_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
+    user_id: Optional[str] = None
     workflow_id: Optional[int] = Field(default=None, foreign_key="workflow.id")
     title: Optional[str] = None
     description: Optional[str] = None
 
 
-class Skill(BaseDBModel, table=True):
+class AgentSkillLink(SQLModel, table=True):
+    agent_id: int = Field(default=None, primary_key=True,
+                          foreign_key="agent.id")
+    skill_id: int = Field(default=None, primary_key=True,
+                          foreign_key="skill.id")
+
+
+class AgentModelLink(SQLModel, table=True):
+    agent_id: int = Field(default=None, primary_key=True,
+                          foreign_key="agent.id")
+    modele_id: int = Field(default=None, primary_key=True,
+                           foreign_key="model.id")
+
+
+class Skill(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.now, sa_column=Column(
+        DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
+    updated_at: datetime = Field(default_factory=datetime.now, sa_column=Column(
+        DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
+    user_id: Optional[str] = None
     title: str
     content: str
     description: Optional[str] = None
     secrets: Optional[Dict] = Field(default={}, sa_column=Column(JSON))
     libraries: Optional[Dict] = Field(default={}, sa_column=Column(JSON))
+    agents: List["Agent"] = Relationship(
+        back_populates="skills", link_model=AgentSkillLink)
 
 
-class Model(BaseDBModel, table=True):
+class Model(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
+    updated_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
+    user_id: Optional[str] = None
     model: str
     api_key: Optional[str] = None
     base_url: Optional[str] = None
     api_type: Optional[str] = None
     api_version: Optional[str] = None
     description: Optional[str] = None
-
-
-class LLMConfig(BaseDBModel, table=False):
-    """ Data model for LLM Config for AutoGen """
-
-    config_list: List[Model] = Field(
-        default_factory=list, sa_column=Column(JSON))
     temperature: float = 0
     cache_seed: Optional[int] = None
     timeout: Optional[int] = None
     max_tokens: Optional[int] = None
+    agents: List["Agent"] = Relationship(
+        back_populates="models", link_model=AgentModelLink)
 
 
-# @dataclass
-# class AgentConfig:
-#     """Data model for Agent Config for AutoGen"""
-
-#     name: str
-#     llm_config: Optional[Union[LLMConfig, bool]] = False
-#     human_input_mode: str = "NEVER"
-#     max_consecutive_auto_reply: int = 10
-#     system_message: Optional[str] = None
-#     is_termination_msg: Optional[Union[bool, str, Callable]] = None
-#     code_execution_config: Optional[Union[bool, str, Dict[str, Any]]] = None
-#     default_auto_reply: Optional[str] = ""
-#     description: Optional[str] = None
-
-class AgentConfig(BaseDBModel, table=False):
+class AgentConfig(SQLModel, table=False):
     name: str
-    llm_config:  Optional[Union[LLMConfig, bool]] = False
     human_input_mode: str = "NEVER"
     max_consecutive_auto_reply: int = 10
     system_message: Optional[str] = None
     is_termination_msg: Optional[Union[bool, str, Callable]] = None
-    code_execution_config: Optional[Dict[str, Any]] = None
+    code_execution_config: Optional[Union[bool, Dict]] = Field(
+        default=False, sa_column=Column(JSON))
     default_auto_reply: Optional[str] = ""
     description: Optional[str] = None
 
 
-class Agent(BaseDBModel, table=True):
-    type: Literal["assistant", "userproxy"]
+class Agent(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
+    updated_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
+    user_id: Optional[str] = None
+    type: str = Field(default_factory=Literal["assistant", "userproxy"])
     config: AgentConfig = Field(
         default_factory=AgentConfig, sa_column=Column(JSON))
-    skills: Optional[List[Skill]] = Field(
-        default_factory=list, sa_column=Column(JSON))
+    skills: List[Skill] = Relationship(
+        back_populates="agents", link_model=AgentSkillLink)
+    models: List[Model] = Relationship(
+        back_populates="agents", link_model=AgentModelLink)
 
 
-class GroupChatConfig(BaseDBModel, table=False):
+class GroupChatConfig(SQLModel, table=False):
     agents: List[Agent] = Field(default_factory=list)
     admin_name: str = "Admin"
     messages: List[Dict] = Field(default_factory=list)
@@ -98,7 +119,7 @@ class GroupChatConfig(BaseDBModel, table=False):
     allow_repeat_speaker: Optional[Union[bool, List[AgentConfig]]] = True
 
 
-class GroupChat(BaseDBModel, table=True):
+class GroupChat(SQLModel, table=False):
     type: Literal["groupchat"]
     config: AgentConfig = Field(
         default_factory=AgentConfig, sa_column=Column(JSON))
@@ -108,11 +129,18 @@ class GroupChat(BaseDBModel, table=True):
         default_factory=list, sa_column=Column(JSON))
 
 
-class Workflow(BaseDBModel, table=True):
+class Workflow(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), server_default=func.now()))  # pylint: disable=not-callable
+    updated_at: datetime = Field(default=datetime.now(), sa_column=Column(
+        DateTime(timezone=True), onupdate=func.now()))  # pylint: disable=not-callable
+    user_id: Optional[str] = None
     name: str
     description: str
     sender: Agent = Field(default_factory=Agent, sa_column=Column(JSON))
     receiver: Union[Agent, GroupChat] = Field(
         default_factory=Union[Agent, GroupChat], sa_column=Column(JSON))
-    type: Literal["twoagents", "groupchat"] = "twoagents"
-    summary_method: Optional[Literal["last", "none", "llm"]] = "last"
+    type: str = Field(default_factory=Literal["twoagents", "groupchat"])
+    # summary_method: Optional[Literal["last", "none", "llm"]] = "last"
+    summary_method: str = Field(default_factory=Literal["last", "none", "llm"])
--- samples/apps/autogen-studio/autogenstudio/models/dbmanager.py
+++ samples/apps/autogen-studio/autogenstudio/models/dbmanager.py
@@ -1,5 +1,4 @@
-from sqlmodel import Session, create_engine, select, delete
-from .db import BaseDBModel
+from sqlmodel import SQLModel, Session, create_engine, select, delete, and_
 from datetime import datetime
 import logging
 
@@ -8,53 +7,67 @@ logger.setLevel(logging.INFO)
 
 
 class DBManager:
+    """ A class to manage database operations"""
+
     def __init__(self, engine_uri: str):
         self.engine = create_engine(engine_uri)
         self.session = Session(self.engine)
 
     def create_db_and_tables(self):
-        BaseDBModel.metadata.create_all(self.engine)
+        """ Create a new database and tables """
+        SQLModel.metadata.create_all(self.engine)
 
-    def upsert(self, model: BaseDBModel):
+    def upsert(self, model: SQLModel):
+        """ Create a new entity"""
         # check if the model exists, update else add
         model_class = type(model)
         current_model = self.session.exec(
             select(model_class).where(model_class.id == model.id)).first()
         if current_model:
             model.updated_at = datetime.now()
-            self.session.update(model)
+            for key, value in model.model_dump().items():
+                setattr(current_model, key, value)
+            self.session.add(current_model)
         else:
             self.session.add(model)
         self.session.commit()
+        status_message = "Item Updated Successfully " if current_model else "Item Added Successfully"
+        return status_message
 
     def _model_to_dict(self, model_obj):
         return {col.name: getattr(model_obj, col.name) for col in model_obj.__table__.columns}
 
-    def get(self, row_id: int, model_class: BaseDBModel, return_json: bool = False):
-        statement = select(model_class).where(model_class.id == row_id)
+    def get(self, model_class: SQLModel, filters: dict = None, return_json: bool = False):
+        """ List all entities for a user"""
+        if filters:
+            conditions = [getattr(model_class, col) ==
+                          value for col, value in filters.items()]
+            statement = select(model_class).where(and_(*conditions))
+        else:
+            statement = select(model_class)
         if return_json:
             return [self._model_to_dict(row) for row in self.session.exec(statement).all()]
         else:
             return self.session.exec(statement).all()
 
-    def get_all(self, model_class: BaseDBModel, return_json: bool = False):
-        statement = select(model_class)
-        if return_json:
-            return [self._model_to_dict(row) for row in self.session.exec(statement).all()]
+    def delete(self, model_class: SQLModel, filters: dict = None):
+        """ Delete an entity"""
+        row = None
+        status_message = ""
+        if filters:
+            conditions = [getattr(model_class, col) ==
+                          value for col, value in filters.items()]
+            row = self.session.exec(
+                select(model_class).where(and_(*conditions))).all()
         else:
-            return self.session.exec(statement).all()
-
-    def delete(self, row_id: int, model_class: BaseDBModel):
-        row = self.session.exec(select(model_class).where(
-            model_class.id == row_id)).one_or_none()
+            row = self.session.exec(select(model_class)).all()
         if row:
-            self.session.delete(row)
+            for row in row:
+                self.session.delete(row)
             self.session.commit()
+            status_message = "Deleted Successfully"
         else:
-            print(f"Row with id {row_id} not found")
-            logger.info("Row with id %s not found", row_id)
-
-    def delete_all(self, model_class: BaseDBModel):
-        result = self.session.exec(delete(model_class))
-        self.session.commit()
-        logger.info("All rows in %s (%s) deleted", model_class, result.rowcount)
+            print(f"Row with filters {filters} not found")
+            logger.info("Row with filters %s not found", filters)
+            status_message = "Row not found"
+        return status_message
